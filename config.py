74ed0bbba30d2a5bef6f94fb7d9db16acdaf37be
BernatMir <a24bermirpre@inspedralbes.cat>
2024-11-10 20:36:09 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+
+db_config = {
+    'user': 'root',          # Nom d'usuari de MySQL
+    'password': 'Bcn2024%', # Contrasenya de MySQL
+    'host': 'localhost',               # Normalment localhost
+    'database': 'tienda'               # Nom de la base de dades
+}
